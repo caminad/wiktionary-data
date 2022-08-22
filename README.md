236f3202fe7dc04f4b6af9044f45f8aa86b6d855
David Jones <david@omg.lol>
2022-08-22 01:18:36 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Wiktionary data
+
+Fetch English Wiktionary titles by number of unicode characters.
+
+## Updating
+
+Requires [Deno](https://deno.land/#installation).
+
+```sh
+> ./scripts/update.ts
+```
---
+++ scripts/update.ts
@@ -0,0 +1,121 @@
+#!/usr/bin/env deno run --allow-net=dumps.wikimedia.org --allow-read=. --allow-write=.
+
+import { readLines } from "https://deno.land/std@0.152.0/io/mod.ts";
+import { readerFromStreamReader } from "https://deno.land/std@0.152.0/streams/mod.ts";
+
+const DEST_DIR = import.meta.resolve("../data/");
+const DUMPS_BASE_URL = "https://dumps.wikimedia.org/enwiktionary/latest/";
+
+if (import.meta.main) {
+  main();
+}
+
+async function main() {
+  const res = await fetch_dump("enwiktionary-latest-all-titles-in-ns0.gz", {
+    etag: await read_etag(DEST_DIR),
+    base_url: DUMPS_BASE_URL,
+  });
+  if (res.status === 304) {
+    // Not Modified
+    return;
+  }
+  const titles = read_titles(res);
+  const grouped_titles = await group_by_code_point_count(titles);
+
+  await ensure_empty(DEST_DIR);
+  await write_grouped_titles(DEST_DIR, grouped_titles);
+  await write_etag(DEST_DIR, res.headers.get("ETag"));
+}
+
+async function read_etag(base_dir: string): Promise<string | null> {
+  try {
+    return await Deno.readTextFile(new URL(".etag", base_dir));
+  } catch (e) {
+    if (e instanceof Deno.errors.NotFound) {
+      return null;
+    } else {
+      throw e;
+    }
+  }
+}
+
+async function write_etag(
+  base_dir: string,
+  etag: string | null,
+): Promise<void> {
+  if (etag === null) {
+    return;
+  }
+  await Deno.writeTextFile(new URL(".etag", base_dir), etag);
+}
+
+async function fetch_dump(
+  name: string,
+  options: { etag: string | null; base_url: string },
+): Promise<Response> {
+  const req = new Request(new URL(name, options.base_url));
+  if (options.etag) {
+    req.headers.set("If-None-Match", options.etag);
+  }
+  const res = await fetch(req);
+  return new Response(
+    res.body?.pipeThrough(new DecompressionStream("gzip")),
+    res,
+  );
+}
+
+async function* read_titles(res: Response): AsyncIterableIterator<string> {
+  if (!res.ok) {
+    throw new Error(`${res.status} ${res.statusText} (${res.url})`);
+  }
+  if (!res.body) {
+    throw new Error(`No body (${res.url})`);
+  }
+
+  const titles = readLines(readerFromStreamReader(res.body.getReader()));
+  await titles.next(); // skip header
+  yield* titles;
+}
+
+async function group_by_code_point_count(
+  strings: AsyncIterable<string>,
+): Promise<Map<number, string[]>> {
+  const groups = new Map<number, string[]>();
+  for await (const s of strings) {
+    const code_point_count = [...s].length;
+    if (code_point_count === 0) {
+      continue;
+    }
+    let group = groups.get(code_point_count);
+    if (!group) {
+      group = [];
+      groups.set(code_point_count, group);
+    }
+    group.push(s);
+  }
+  return groups;
+}
+
+async function write_grouped_titles(
+  dest_dir: string,
+  grouped_titles: Map<number, string[]>,
+): Promise<void> {
+  await Promise.all(Array.from(grouped_titles, ([count, titles]) => {
+    const path = new URL(`${String(count).padStart(3, "0")}.txt`, dest_dir);
+    return Deno.writeTextFile(path, titles.map((s) => s + "\n").join(""));
+  }));
+}
+
+async function ensure_empty(dir: string): Promise<void> {
+  try {
+    for await (const f of Deno.readDir(dir)) {
+      await Deno.remove(new URL(f.name, dir));
+    }
+  } catch (e) {
+    if (e instanceof Deno.errors.NotFound) {
+      await Deno.mkdir(dir, { recursive: true });
+    } else {
+      throw e;
+    }
+  }
+}
